b1cf4b0dfb1f1964487235a686fb2077f9c4604f
Fernando <afeureis@gmail.com>
2021-06-04 16:49:23 -0300

    TESTANDO AINDA

--- appcurso.js
+++ appcurso.js
@@ -1,22 +1,78 @@
 
+//const result = document.querySelector('.result');
 
+//const fetchData = async () => {
+//try {
+
+ //const {data} = await axios.get('/.netlify/functions/1-hello');
+//console.log(data);
+    //result.textContent = data
+    
+//} catch (error) {
+    //console.log(erro.response);
+
+    
+//}
+    
+
+
+//}
+
+//fetchData()
+
+const formAPI = document.querySelector('.formAPI')
+const inPlaca = document.querySelector('.inPlaca')
+const inRenavam = document.querySelector('.inRenavam')
 const result = document.querySelector('.result');
 
-const fetchData = async () => {
-try {
 
-    const {data} = await axios.get('/.netlify/functions/1-hello');
-    console.log(data);
-    result.textContent = data
-    
-} catch (error) {
-    console.log(erro.response);
 
+function success(pos){
+
+    formAPI.addEventListener('submit',(e)=>{
+        event.preventDefault()
+        let placa = inPlaca.value 
+        let renavam = inRenavam.value 
+        const options = {
+          method: 'GET',
+          mode: 'cors',
+          cache: 'defaut'
+        }
+
+        
     
+
+    fetch (`/netlify/functions/1-hello?placa=${placa}&renavam=${renavam}`, options)
+    .then(response=>response.json())
+    .then(data => show(data))
+
+  })
+  .catch(e => console.log('Deu erro'))
+
 }
-    
 
 
-}
 
-fetchData()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+   
+
+
+
--- functions/1-hello.js
+++ functions/1-hello.js
@@ -1,12 +1,20 @@
 require('dotenv').config()
 
+const axios = require("axios");
 
+exports.handler = async function(event, context, callback) {
 
-exports.handler = async (event, context)=>{
-    return{
-        statusCode:200,
-        body: 'Aqui vai a tabela do detran mudanaxa!!!',
-
-    }
+  const placa = "FQO0023"
+  const renavam = "01053051317"
+  const url = `https://api.infosimples.com/api/v1/detran/sp/debitos.json?token=${process.env.API_KEY}&timeout=600&placa=${placa}&renavam=${renavam}`;
+ 
+  const response = await axios.get(url);
+ 
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(response.data)
+  });
 
 }
+
+
