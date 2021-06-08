1c20fe156e3a5f512dd3584412221f4ef5997e10
Fernando <afeureis@gmail.com>
2021-06-08 12:30:02 -0300

    configuradopesquisa

--- appcurso.js
+++ appcurso.js
@@ -60,17 +60,56 @@ async function getPlacaRenavam(placa, renavam) {
 
     const { ultimo_licenciamento } = data.data;
 
+    const { bloqueio_furto } = data.data;
 
-    result.innerHTML = `<hr> <h6>IPVA: ${ ipva }</h6>
-    <hr> <h6>Multas: ${ multas_total }</h6>
-    <hr> <h6>Status do Licenciamento: ${ status_licenciamento }</h6>
-    <hr> <h6>Último Licenciamento: ${ ultimo_licenciamento }</h6>`
+    const { restricao_administrativa } = data.data;
+
+    const { restricao_financeira } = data.data;
+
+    const { restricao_judiciaria } = data.data;
+
+    const { restricao_tributaria } = data.data;
+
+    const { registro_guincho } = data.data;
+
+    const { inspecao_gnv } = data.data;
+
+
+
+    result.innerHTML = `
+    <hr><h6> <strong> Dados do Veículo </strong> </h6>
+    <h6>Placa: <strong> ${placa} </strong> Renavam: <strong> ${renavam} </strong> </h6>
+    
+    <hr> <h6><strong>IPVA</strong></h6>
+    <h6>${ipva}</h6>
+
+    <hr> <h6><strong>Multas</strong></h6>
+    <h6>${ multas_total }</h6>
+
+    <hr> <h6><strong>Licenciamento</strong></h6>
+    <h6><strong>Status do Licenciamento:</strong> ${ status_licenciamento }</h6>
+    <h6><strong>Último Licenciamento:</strong> ${ ultimo_licenciamento }</h6>
+
+    <hr> <h6><strong>Restrições</strong></h6>
+    <h6><strong>Bloqueio de Furto/Roubo</strong> ${ bloqueio_furto }</h6>
+    <h6><strong>Administrativas</strong> ${ restricao_administrativa }</h6>
+    <h6><strong>Financeira</strong> ${ restricao_financeira }</h6>
+    <h6><strong>Judiciária</strong> ${ restricao_judiciaria }</h6>
+    <h6><strong>Tributária</strong> ${ restricao_tributaria }</h6>
+    <h6><strong>Guincho</strong> ${ registro_guincho }</h6>
+
+    <hr> <h6><strong>Inspeção Veicular</strong></h6>
+    <h6><strong>Inspeção GNV</strong> ${ inspecao_gnv }</h6>
+
+    `
     
     
     
 
 
     console.log('data :', data);
+
+   
 
     
 
